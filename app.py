01edcafa226bdc5f4cd13ccf5274f9b582ff5194
Prism <ershubhcsofficial@gmail.com>
2025-03-24 00:38:14 +0530

    Bug Fixes in app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # main application file
 
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-from chat_model import StudyBotAI
-from reminder_manager import ReminderManager
+from chatModel import StudyBotAI
+from reminderManager import ReminderManager
 import os
 from dotenv import load_dotenv
 
