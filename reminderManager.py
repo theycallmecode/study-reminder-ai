c9a9eaa4900cf9371b0fcdc3700a119580b0e674
Prism <ershubhcsofficial@gmail.com>
2025-03-23 23:25:06 +0530

    Included required modules in reminderManager

--- reminderManager.py
+++ reminderManager.py
@@ -1,1 +1,10 @@
 # handle scheduling function and reminder
+
+import schedule
+import time
+import threading
+import datetime
+import json
+import os
+from datetime import datetime, timedelta
+import pyttsx3
