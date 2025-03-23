54fbe382b87754fa46c39b25742223dd58b0ec00
Prism <ershubhcsofficial@gmail.com>
2025-03-24 00:24:33 +0530

    Imported required modules in app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 # main application file
+
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from chat_model import StudyBotAI
+from reminder_manager import ReminderManager
+import os
+from dotenv import load_dotenv
