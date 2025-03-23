631169aac9457e0630f895a6e48106328c2e6d94
Prism <ershubhcsofficial@gmail.com>
2025-03-23 22:53:59 +0530

    Imported required Modules

--- chatModel.py
+++ chatModel.py
@@ -1,1 +1,5 @@
-# handle all AI conversation
+import os
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
